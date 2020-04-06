04255624386ff4f553933b5790801ac6649e36a3
allan <luciocorreia.allan@gmail.com>
2020-04-06 14:18:05 -0300

    Camera taken photos

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:contacts/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
   final Contact contact;
@@ -69,7 +70,17 @@ class _ContactPageState extends State<ContactPage> {
                         image: _editedContact.img != null
                             ? FileImage(File(_editedContact.img))
                             : AssetImage("images/person.png"))),
-              )),
+              ),
+              onTap: (){
+                  ImagePicker.pickImage(source: ImageSource.camera).then((file){
+                    if(file == null) return;
+                    setState(() {
+                      _editedContact.img = file.path;  
+                    });
+                    
+                  });
+              },
+              ),
               TextField(
                 focusNode: _nameFocus,
                 decoration: InputDecoration(labelText: "Nome"),
